6caac82bc530cbbcacb5789259cc81172de35f83
Tomek A <tomus.adamczyk@gmail.com>
2019-08-03 10:57:36 +0200

    Task update

--- src/containers/Tasks/FullTask.js
+++ src/containers/Tasks/FullTask.js
@@ -56,14 +56,14 @@ const GET_TASK = gql`
 class FullTask extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            updateInfo: null
-        }
-
         this.statusId = React.createRef();
         this.priorityId = React.createRef();
         this.description = React.createRef();
+        this.name = React.createRef();
         this.updateInformation = null;
+        this.state = {
+            isFirstRender: true
+        }
     }
 
     updateTask = () => {
@@ -81,59 +81,106 @@ class FullTask extends Component {
     updateStatus = () => {
         this.updateTask();
         this.updateInformation = 'Status';
-        this.setState({updateInfo: this.updateInformation});
+        this.setState({status: this.statusId.value});
     }
 
     updatePriority = () => {
         this.updateTask();
         this.updateInformation = 'Priority';
-        this.setState({updateInfo: this.updateInformation});
+        this.setState({priority: this.priorityId.value});
     }
 
-    updateName = () => { 
-        if(this.name.value) {
+    updateName = (e) => { 
+        if(e.target.value !== this.state.name) {
             this.updateTask();
             this.updateInformation = 'Task name';
+            this.setState({name: this.name.value});
         }
     }
 
-    updateDescription = () => { 
-        if(this.description.value) {
+    updateDescription = (e) => { 
+        if(e.target.value !== this.state.description) {
             this.updateTask();
             this.updateInformation = 'Description';
+            this.setState({description: this.description.value});
+            console.log(this.state)
         }
     }
+
+    
+    setData = (name, description, status, priority) => {
+        if (this.state.isFirstRender){
+            this.setState({name, description, status, priority, isFirstRender: false})
+        }
+    }
+
+    componentDidMount() {
+        console.log(this.state)
+    }
+    componentDidUpdate() {
+        console.log(this.state)
+    }
+
+    componentWillUnmount() {
+        this.setState({isFirstRender: true})
+    }
+    
 
     render() {   
         
-    const FullTaskData = (props) => {    
-        return(
-            <ContentTable>
-                {this.updateInformation ? <InfoBox info={this.updateInformation}></InfoBox> : null}
-                <LeftColumn>
-                    <h2>Task</h2>
-                    <Input blur={props.updateName} type="text" placeholder={props.name} defaultValue={props.name} ref={input => this.name = input}/>
-                    <TextArea updateDescription={props.updateDescription} type="text" placeholder={props.description} defaultValue={props.description} ref={input => this.description = input}></TextArea>
-                </LeftColumn>
-                <CenterColumn>
-                    <h2>List of tasks</h2>
-                </CenterColumn>
-                <RightColumn>
+        const FullTaskData = (props) => {    
+            return(
+                <ContentTable>
+                    {this.updateInformation ? <InfoBox updateTask={props.updateTask} info={this.updateInformation}></InfoBox> : null}
 
-                    <h3>Status</h3>
-                    <Statuses updateStatus={props.updateStatus} statusId={props.statusId} status={props.status} ref={input => this.statusId = input}/>
+                    <LeftColumn>
+                        <h2>Task</h2>
+                        <Input 
+                        blur={(e) => props.updateName(e)} 
+                        type="text" 
+                        placeholder={props.name} 
+                        defaultValue={this.state.name ? this.state.name : props.name} 
+                        ref={input => this.name = input}/>
 
-                    <h3>Priority</h3>
-                    <Priorities updatePriority={props.updatePriority} priorityId={props.priorityId} priority={props.priority} ref={input => this.priorityId = input}/>
+                        <TextArea 
+                        updateDescription={(e) => props.updateDescription(e)} 
+                        type="text" 
+                        placeholder={props.description} 
+                        defaultValue={this.state.description ? this.state.description : props.description} 
+                        ref={input => this.description = input}></TextArea>
+                    </LeftColumn>
 
-                    <h3>Client</h3>
-                    <p>{props.client}</p>
-                </RightColumn>
-            </ContentTable>
-        )
-    }
+                    <CenterColumn>
+                        <h2>List of tasks</h2>
+                    </CenterColumn>
+
+                    <RightColumn>
+                        <h3>Status</h3>
+                        <Statuses 
+                        updateStatus={props.updateStatus} 
+                        statusId={props.statusId} 
+                        status={props.status} 
+                        ref={input => this.statusId = input}/>
+
+                        <h3>Priority</h3>
+                        <Priorities 
+                        updatePriority={props.updatePriority} 
+                        priorityId={props.priorityId} 
+                        priority={props.priority} 
+                        ref={input => this.priorityId = input}/>
+
+                        <h3>Client</h3>
+                        <p>{props.client}</p>
+                    </RightColumn>
+                    
+                </ContentTable>
+            )
+        }
         return (
-            <Query query={GET_TASK} variables={{id: Number(this.props.match.params.id)}}>
+            <Query 
+                query={GET_TASK} 
+                variables={{id: Number(this.props.match.params.id)}} 
+                onCompleted={data => this.setData(data.task.name, data.task.description, data.task.statusId.name, data.task.priorityId.name)}>
                 {({loading, error, data, refetch} ) => {
                     if(loading) return <Spinner />;
                     if(error) return <p>Nie mogę pobrać listy zadań</p>;
@@ -153,6 +200,7 @@ class FullTask extends Component {
                         updatePriority={this.updatePriority}
                         updateName={this.updateName}
                         updateDescription={this.updateDescription}
+                        updateTask={this.updateTask}
                         />
                     )
                 }}
